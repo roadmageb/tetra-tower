cc0d2fbde8f7fc1efcde51ac2063cec5270d9380
Dhkim <kimdhyungg@gmail.com>
2020-01-17 22:00:23 +0900

    code indentation

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -7,19 +7,22 @@ public class Map : MonoBehaviour
     // Start is called before the first frame update
     public static int count = 0;
 
-
     public static int gridWidth = 10;
     public static int gridHeight = 20;
     public static Transform[,] grid = new Transform[gridWidth, gridHeight];
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         SpawnNextTetromino();
     }
 
-    public bool isFullRowAt(int y) {
-        for (int x = 0; x < gridWidth; ++x) {
-            if (grid[x, y] == null) {
+    public bool isFullRowAt(int y)
+    {
+        for (int x = 0; x < gridWidth; ++x)
+        {
+            if (grid[x, y] == null)
+            {
                 return false;
             }
         }
@@ -27,16 +30,21 @@ public class Map : MonoBehaviour
         return true;
     }
 
-    public void DeleteMinoAt(int y) {
-        for (int x = 0; x < gridWidth; ++x) {
+    public void DeleteMinoAt(int y)
+    {
+        for (int x = 0; x < gridWidth; ++x)
+        {
             Destroy(grid[x, y].gameObject);
             grid[x, y] = null;
         }
     }
 
-    public void MoveRowDown(int y) {
-        for (int x = 0; x < gridWidth; ++x) {
-            if (grid[x, y] != null) {
+    public void MoveRowDown(int y)
+    {
+        for (int x = 0; x < gridWidth; ++x)
+        {
+            if (grid[x, y] != null)
+            {
                 grid[x, y - 1] = grid[x, y];
                 grid[x, y] = null;
                 grid[x, y - 1].position += new Vector3(0, -1, 0);
@@ -44,66 +52,85 @@ public class Map : MonoBehaviour
         }
     }
 
-    public void MoveAllRowsDown(int y) {
-        for (int i = y; i < gridHeight; ++i) {
+    public void MoveAllRowsDown(int y)
+    {
+        for (int i = y; i < gridHeight; ++i)
+        {
             MoveRowDown(i);
         }
     }
 
-    public void DeleteRow() {
-        for (int y = 0; y < gridHeight; ++y) {
-            if (isFullRowAt(y)) {
+    public void DeleteRow()
+    {
+        for (int y = 0; y < gridHeight; ++y)
+        {
+            if (isFullRowAt(y))
+            {
                 DeleteMinoAt(y);
 
                 MoveAllRowsDown(y + 1);
 
                 --y;
-            } else {
+            }
+            else
+            {
                 //Debug.Log($"{y} is not full\n");
             }
         }
     }
 
-    public void UpdateGrid(Tetromino tetromino) {
-        for (int y = 0; y < gridHeight; ++y) {
-            for (int x = 0; x < gridWidth; ++x) {
-                if (grid[x, y] != null) {
-                    if (grid[x, y].parent == tetromino.transform) {
+    public void UpdateGrid(Tetromino tetromino)
+    {
+        for (int y = 0; y < gridHeight; ++y)
+        {
+            for (int x = 0; x < gridWidth; ++x)
+            {
+                if (grid[x, y] != null)
+                {
+                    if (grid[x, y].parent == tetromino.transform)
+                    {
                         grid[x, y] = null;
                     }
                 }
             }
         }
 
-        foreach (Transform mino in tetromino.transform) {
+        foreach (Transform mino in tetromino.transform)
+        {
             Vector2 pos = Round(mino.position);
 
-            if (pos.y < gridHeight) {
+            if (pos.y < gridHeight)
+            {
                 grid[(int)pos.x, (int)pos.y] = mino;
             }
         }
     }
 
-    public Transform GetTransformAtGridPosition(Vector3 pos) {
-
-        if (pos.y > gridHeight - 1) {
+    public Transform GetTransformAtGridPosition(Vector3 pos)
+    {
+        if (pos.y > gridHeight - 1)
+        {
             return null;
-        } else {
+        }
+        else
+        {
             return grid[(int)pos.x, (int)pos.y];
         }
     }
 
-    public void SpawnNextTetromino() {
+    public void SpawnNextTetromino()
+    {
         GameObject tetSpawn = GameObject.Find("TetrominoSpawnerObj");
         tetSpawn.GetComponent<TetrominoSpawner>().spawnShuffled();
-
     }
 
-    public bool CheckIsInsideGrid(Vector3 pos) {
+    public bool CheckIsInsideGrid(Vector3 pos)
+    {
         return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
     }
 
-    public Vector3 Round(Vector3 pos) {
+    public Vector3 Round(Vector3 pos)
+    {
         return new Vector3((int)Mathf.Round(pos.x), (int)Mathf.Round(pos.y), 0);
     }
 }

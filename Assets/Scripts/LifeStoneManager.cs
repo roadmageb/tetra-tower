e8dc2a5dfd89e10f39a7815199e2e26ba17c9e16
Merseong <esc990720@korea.ac.kr>
2020-01-21 16:08:58 +0900

    무작위 모양의 생명석 정보를 string으로 만들게 함

--- Assets/Scripts/LifeStoneManager.cs
+++ Assets/Scripts/LifeStoneManager.cs
@@ -18,9 +18,54 @@ public class LifeStoneManager : Singleton<LifeStoneManager>
 
     }
 
+    /// <summary>
+    /// Create randomized shape of life stone.
+    /// </summary>
+    /// <param name="amount">Total amount of life stone</param>
+    /// <param name="goldAmount">Amount of gold life stone</param>
+    /// <returns>Encoded string of shape of created life stone</returns>
     private string CreateLifeStoneShape(int amount, int goldAmount = 0)
     {
-        return null;
+        int xSize = Mathf.Min(3, amount), ySize = amount;
+        bool[,] lifeStonePos = new bool[ySize, xSize];
+        lifeStonePos[Random.Range(0, ySize), Random.Range(0, xSize)] = true;
+
+        List<Vector2Int> cands = new List<Vector2Int>();
+        for (int i = 1; i < amount; i++)
+        {
+            for (int y = 0; y < ySize; y++)
+            {
+                for(int x = 0; x < xSize; x++)
+                {
+                    if(!lifeStonePos[y, x] && ((x > 0 && lifeStonePos[y, x - 1]) || (x < xSize - 1 && lifeStonePos[y, x + 1]) || (y > 0 && lifeStonePos[y - 1, x]) || (y < ySize - 1 && lifeStonePos[y + 1, x])))
+                    {
+                        cands.Add(new Vector2Int(x, y));
+                    }
+                }
+            }
+            if (cands.Count == 0) break;
+            int randomIndex = Random.Range(0, cands.Count);
+            lifeStonePos[cands[randomIndex].y, cands[randomIndex].x] = true;
+            cands.Clear();
+        }
+
+        string result = "";
+        for (int y = 0; y < ySize; y++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                if(lifeStonePos[y, x])
+                {
+                    result += ((int)LifeStoneType.Normal).ToString();
+                }
+                else
+                {
+                    result += ((int)LifeStoneType.NULL).ToString();
+                }
+            }
+        }
+
+        return result;
     }
 
     public void ExpandRow(int row)
@@ -64,11 +109,18 @@ public class LifeStoneManager : Singleton<LifeStoneManager>
     void Start()
     {
         InitiateLifeStone(6, 3);
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            string temp = CreateLifeStoneShape(3, 0);
+            Debug.Log(temp[0] + "" + temp[1] + "" + temp[2]);
+            Debug.Log(temp[3] + "" + temp[4] + "" + temp[5]);
+            Debug.Log(temp[6] + "" + temp[7] + "" + temp[8]);
+        }
     }
 }

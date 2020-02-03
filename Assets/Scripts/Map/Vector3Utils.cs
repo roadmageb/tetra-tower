7871b7af5bb4ca5f1bfeb35c8a72f89d0d3e1002
Dhkim <kimdhyungg@gmail.com>
2020-02-04 08:32:40 +0900

    Add Vector3 utility functions

---
+++ Assets/Scripts/Map/Vector3Utils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate float FuncToMap(float x);
+
+public static class Vector3Utils
+{
+    public static Vector3 Map(Vector3 vec, FuncToMap func)
+    {
+        return new Vector3(func(vec.x), func(vec.y), func(vec.z));
+    }
+
+    public static Vector3 newVector3From(Vector3 v)
+    {
+        return new Vector3(v.x, v.y, v.z);
+    }
+
+}

bb79d5319840c78c878a5c43cda379f6d2b264d6
Dhkim <kimdhyungg@gmail.com>
2020-02-05 12:25:27 +0900

    Bugfix: Adjust piston y position to -1 (cause: if 0 then overlapped by mino)

--- Assets/Scripts/Map/PistonSpawner.cs
+++ Assets/Scripts/Map/PistonSpawner.cs
@@ -6,10 +6,13 @@ public class PistonSpawner : MonoBehaviour
 {
     public GameObject PistonLeftPrefab;
     public GameObject PistonRightPrefab;
+
+    PistonLeft pistonLeft;
+    PistonRight pistonRight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -35,13 +38,13 @@ public class PistonSpawner : MonoBehaviour
 
     public void spawnLeftNth(int n)
     {
-        GameObject pistonLeftObj = Instantiate(PistonLeftPrefab, new Vector3(-7.5f, (float)n, 0), Quaternion.identity) as GameObject;
-        PistonLeft pistonLeft = pistonLeftObj.GetComponent<PistonLeft>();
+        GameObject pistonLeftObj = Instantiate(PistonLeftPrefab, new Vector3(-7.5f, (float)n, -1), Quaternion.identity) as GameObject;
+        pistonLeft = pistonLeftObj.GetComponent<PistonLeft>();
     }
 
     public void spawnRightNth(int n)
     {
-        GameObject pistonRightObj = Instantiate(PistonRightPrefab, new Vector3(16.5f, (float)n, 0), Quaternion.identity) as GameObject;
-        PistonRight pistonRight = pistonRightObj.GetComponent<PistonRight>();
+        GameObject pistonRightObj = Instantiate(PistonRightPrefab, new Vector3(16.5f, (float)n, -1), Quaternion.identity) as GameObject;
+        pistonRight = pistonRightObj.GetComponent<PistonRight>();
     }
-}
+} 

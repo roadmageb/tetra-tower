a6bc61fd2e9201555dcd6f6385c1fc3f3818c876
jasson31 <jasson31@naver.com>
2020-02-08 20:49:30 +0900

    플레이어 추적하는 적 코드 wip

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform target;
+    public float speed = 200;
+    public float nextWayPointDistance = 3;
+
+    Path path;
+    int currentWaypoint = 0;
+    bool reachedEndOfPath = false;
+    Seeker seeker;
+    Rigidbody2D rb;
+
     public float maxHP;
     public float currentHP;
     public EnemyCtrl enemyCtrl;
@@ -20,6 +31,73 @@ public class Enemy : MonoBehaviour
 
     public void Death()
     {
+        Debug.Log("I die");
+    }
 
+    void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void SeekTarget()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            reachedEndOfPath = true;
+            return;
+        }
+        else
+        {
+            reachedEndOfPath = false;
+        }
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        Vector2 force = direction * speed;
+
+        rb.AddForce(force);
+        if (force.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (force.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+
+        if(distance < nextWayPointDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+
+    void UpdatePath()
+    {
+        if (seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    private void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0, 0.1f);
+    }
+
+    private void LateUpdate()
+    {
+        SeekTarget();
     }
 }

04f1ab8a75c2cfd5a1bfa128dddf9ba7aef69adb
Dhkim <kimdhyungg@gmail.com>
2020-02-17 15:05:13 +0900

    Test: GridUtils Test codes

---
+++ Assets/Scripts/Map/Test/TestGridUtils.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestGridUtils : MonoBehaviour
+{
+
+    GridUtils gu;
+
+    Transform[,] emptyGrid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        emptyGrid = new Transform[10,9];
+        gu = new GridUtils();
+        gu.Initialize(emptyGrid);
+        gu.Test();
+
+    }
+}

9126e5bceb65f289d4735031b87ffb1f56c3216a
Dhkim <kimdhyungg@gmail.com>
2020-02-21 21:25:49 +0900

    Still debugging: DebugDelete function

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -1,62 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.CompilerServices;
 
 public class Map : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject tetrominoSpawnerPrefab;
+    public GameObject pistonSpawnerPrefab;
+    public GameObject debugMapPrefab;
+
+    TetrominoSpawner tetrominoSpawner;
+    PistonSpawner pistonSpawner;
+    DebugMap debugMap;
+
     public static int count = 0;
 
-    public static int gridWidth = 10;
-    public static int gridHeight = 20;
-    public static Transform[,] grid = new Transform[gridWidth, gridHeight];
-    public static GridUtils gridUtils = new GridUtils();
-    TetrominoSpawner tetSpawner;
-    PistonSpawner pistonSpawner;
+    public const int gridWidth = 10;
+    public const int gridHeight = 20;
+    public Transform[,] grid;
+    public GridUtils gridUtils = new GridUtils();
     RowSlider rowSlider;
 
-    // Start is called before the first frame update
+    public Vector3Int basePosition;
+
     void Start()
     {
-        var tetSpawnerObj = GameObject.Find("TetrominoSpawnerObj");
-        tetSpawner = tetSpawnerObj.GetComponent<TetrominoSpawner>();
 
-        var pistonSpawnerObj = GameObject.Find("PistonSpawnerObj");
-        pistonSpawner= pistonSpawnerObj.GetComponent<PistonSpawner>();
+        basePosition = new Vector3Int(1, 1, 0);
+
+        tetrominoSpawner = Instantiate(tetrominoSpawnerPrefab).GetComponent<TetrominoSpawner>();
+        tetrominoSpawner.Initialize(this);
+
+        pistonSpawner = Instantiate(pistonSpawnerPrefab).GetComponent<PistonSpawner>();
+        pistonSpawner.Instantiate();
+
+        debugMap = GameObject.Find("DebugMap").GetComponent<DebugMap>();
+
+        grid = new Transform[gridWidth, gridHeight];
 
         gridUtils.Initialize(grid);
 
         rowSlider = new GameObject().AddComponent<RowSlider>();
-        rowSlider.Initialize(gridUtils);
+        rowSlider.name = "RowSlider";
+        rowSlider.Initialize(this);
 
         SpawnNextTetromino();
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            reportError();
-        }
-    }
-
-    void reportError()
-    {
-        printGrid();
-        Debug.Log(rowSlider.coroutineCount);
-    }
-
-    public void DestroyRow(int y)
-    {
-        for (int x = 0; x < gridWidth; ++x)
-        {
-            Destroy(grid[x, y].gameObject);
-            grid[x, y] = null;
-        }
-    }
-
     public void DestroyRowsIfFull(bool[] isFull)
     {
+
+        void DestroyRow(int y)
+        {
+            for (int x = 0; x < gridWidth; ++x)
+            {
+                Destroy(grid[x, y].gameObject);
+                grid[x, y] = null;
+            }
+        }
+
         for (int i = 0; i < isFull.Length; ++i)
         {
             if (isFull[i])
@@ -67,22 +69,25 @@ public class Map : MonoBehaviour
     }
 
 
-    public void MoveRowDown(int y, int num)
-    {
-        for (int x = 0; x < gridWidth; ++x)
-        {
-            if (grid[x, y] != null)
-            {
-                grid[x, y - num] = grid[x, y];
-                grid[x, y] = null;
-                // replaced by rowSlider
-                //grid[x, y - num].position += new Vector3(0, -num, 0);
-            }
-        }
-    }
-
     public void MoveAllRowsDown(bool[] isFull, int[] shiftAmount)
     {
+        void MoveRowDown(int y, int num)
+        {
+            for (int x = 0; x < gridWidth; ++x)
+            {
+                if (grid[x, y] != null)
+                {
+                    // for delayed transform shift
+                    var mino = grid[x, y].gameObject.GetComponent<Mino>();
+                    mino.fallDestination = y - num;
+
+                    grid[x, y - num] = grid[x, y];
+                    grid[x, y] = null;
+
+                }
+            }
+        }
+
         for (int i = 1; i < gridHeight; ++i)
         {
             if (!isFull[i]) {
@@ -91,14 +96,14 @@ public class Map : MonoBehaviour
         }
     }
 
-    IEnumerator waitAndDelete(bool[] isFull)
+    IEnumerator WaitAndDelete(bool[] isFull)
     {
         pistonSpawner.spawnIfFull(isFull);
 
         yield return null;
         while (Piston.pistonCount != 0)
         {
-            yield return null;
+            yield return null; 
         }
 
         DestroyRowsIfFull(isFull);
@@ -115,14 +120,40 @@ public class Map : MonoBehaviour
 
     public void RemoveRowsIfFull()
     {
+
+        IEnumerator DebugDelete(bool[] isFull)
+        {
+            pistonSpawner.spawnIfFull(isFull);
+            yield return null;
+
+            while (Piston.pistonCount != 0)
+            {
+                yield return null; 
+            }
+
+            DestroyRowsIfFull(isFull);
+            MoveAllRowsDown(isFull, gridUtils.shiftDown);
+
+            rowSlider.slideDown();
+            yield return null;
+
+            while (rowSlider.coroutineCount != 0)
+            {
+                yield return null;
+            }
+        }
+
         gridUtils.isFullUpdate();
         gridUtils.shiftAmountUpdate();
 
         if (gridUtils.fullRowCount > 0)
         {
-            StartCoroutine(waitAndDelete(gridUtils.isFull));
+            //StartCoroutine(WaitAndDelete(gridUtils.isFull));
+            StartCoroutine(DebugDelete(gridUtils.isFull));
         }
     }
+
+
     public void UpdateGrid(Tetromino tetromino)
     {
         for (int y = 0; y < gridHeight; ++y)
@@ -139,64 +170,41 @@ public class Map : MonoBehaviour
             }
         }
 
-        foreach (Transform mino in tetromino.transform)
+        foreach (Transform child in tetromino.transform)
         {
-            Vector2 pos = Round(mino.position);
+            var mino = child.gameObject.GetComponent<Mino>();
+            //Vector3 pos = Vector3Utils.Map(RelativePosition(mino.position), Mathf.Round);
+            var pos = mino.GetGridPosition();
 
             if (pos.y < gridHeight)
             {
-                grid[(int)pos.x, (int)pos.y] = mino;
+                grid[pos.x, pos.y] = child;
             }
         }
         //printGrid();
     }
 
-    public Transform GetTransformAtGridPosition(Vector3 pos)
+    public Transform GetTransformAtGridPosition(Vector3Int pos)
     {
         if (pos.y > gridHeight - 1)
         {
-            return null;
+            throw new System.Exception("y too high");
+            //return null;
         }
         else
         {
-            return grid[(int)pos.x, (int)pos.y];
+            return grid[pos.x, pos.y];
         }
     }
 
     public void SpawnNextTetromino()
     {
-        tetSpawner.spawnShuffled();
+        tetrominoSpawner.spawnShuffled();
     }
 
-    public bool CheckIsInsideGrid(Vector3 pos)
+    public bool CheckIsInsideGrid(Vector3Int pos)
     {
-        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+        return (pos.x >= 0 && pos.x < gridWidth && pos.y >= 0);
     }
 
-    public Vector3 Round(Vector3 pos)
-    {
-        return new Vector3((int)Mathf.Round(pos.x), (int)Mathf.Round(pos.y), 0);
-    }
-
-    public void printGrid()
-    {
-        string gridStr = "";
-        for( int y = gridHeight - 1; y >= 0; --y)
-        {
-            for (int x = 0; x < gridWidth; ++x)
-            {
-                if (grid[x, y] == null)
-                {
-                    gridStr += "-";
-                }
-                else
-                {
-                    gridStr += "*";
-                }
-            }
-            gridStr += '\n';
-        }
-
-        Debug.Log(gridStr);
-    }
-}
+} 

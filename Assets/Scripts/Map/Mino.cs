aa687e44992bcae7b56bcb3121894dcd2b019933
Dhkim <kimdhyungg@gmail.com>
2020-02-21 21:21:24 +0900

    Add Mino

---
+++ Assets/Scripts/Map/Mino.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mino : MonoBehaviour
+{
+    public bool toBeDestroyed;
+
+    [SerializeField]
+    public int fallDestination;
+
+    public Vector3Int localPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.localPosition = localPosition;
+    }
+
+    public Vector3Int GetGridPosition()
+    {
+        return GetComponentInParent<Tetromino>().gridPosition + localPosition;
+    }
+
+}

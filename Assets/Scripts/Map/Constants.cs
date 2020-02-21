fd69792f5ed07eb5152746e2fbc50c59b8b77bc9
Dhkim <kimdhyungg@gmail.com>
2020-02-21 21:24:23 +0900

    minor: no need for Constants

--- Assets/Scripts/Map/Constants.cs
+++ Assets/Scripts/Map/Constants.cs
@@ -7,7 +7,4 @@ public static class Constants
     public static Vector2 LeftmostBottommostMinoPosition = new Vector2(0, 0);
     public static Vector2 Base = LeftmostBottommostMinoPosition;
     public static float CenterXAxis = Base.x + 4.5f;
-
-    public static int gridHeight = 20;
-    public static int gridWidth = 10;
 }

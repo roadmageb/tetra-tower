f63bd32cafac5f9c3b6082c9fbbaf883cddc2b33
jasson31 <jasson31@naver.com>
2020-02-26 01:16:06 +0900

    플레이어가 문에 닿으면 서서히 다음 방으로 넘어감

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,10 +8,10 @@ public class CameraController : Singleton<CameraController>
     {
         Vector3 from = transform.position;
         Vector3 to = new Vector3(_dest.x, _dest.y, transform.position.z);
-        for (float timer = 0; timer < 0.2f; timer += Time.deltaTime)
+        for (float timer = 0; timer < Room.roomMoveTime; timer += Time.deltaTime)
         {
             yield return null;
-            transform.position = Vector3.Lerp(from, to, timer / 0.2f);
+            transform.position = Vector3.Lerp(from, to, timer / Room.roomMoveTime);
         }
         transform.position = to;
     }
--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    public static float roomMoveTime = 0.2f;
     //For test
     public Room nextRoom;
     //For test
 
+    public Door[] doors;
+
     public void MovePlayerToRoom(Vector2 dir)
     {
         Vector2 destination = (Vector2)PlayerController.Instance.transform.position + dir * 2;
-        PlayerController.Instance.transform.position = destination;
         StartCoroutine(CameraController.Instance.MoveCamera(nextRoom.transform.position));
+        StartCoroutine(PlayerController.Instance.MovePlayer(destination));
+
         GameManager.Instance.aStarPath.transform.position = nextRoom.transform.position;
     }
-
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame

ee42977ef38493d66d7a5110f5b95e72ee2bfd93
jasson31 <jasson31@naver.com>
2020-02-26 18:42:11 +0900

    이제 방안의 모든 적이 죽으면 방이 클리어된 것으로 판정하게 함

--- Assets/Scripts/Room/Room.cs
+++ Assets/Scripts/Room/Room.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    public static float roomMoveTime = 0.2f;
     //For test
     public Room nextRoom;
     //For test
 
+    public bool roomCleared = false;
+    public Transform enemy = null;
+    public static float roomMoveTime = 0.2f;
     public Door[] doors;
 
     public void MovePlayerToRoom(Vector2 dir)
@@ -16,19 +18,23 @@ public class Room : MonoBehaviour
         Vector2 destination = (Vector2)PlayerController.Instance.transform.position + dir * 2;
         StartCoroutine(CameraController.Instance.MoveCamera(nextRoom.transform.position));
         StartCoroutine(PlayerController.Instance.MovePlayer(destination));
+        PlayerController.Instance.transform.parent = nextRoom.transform;
 
         GameManager.Instance.aStarPath.transform.position = nextRoom.transform.position;
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void ClearRoom()
     {
-
+        roomCleared = true;
+        Debug.Log("Room Cleared");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!roomCleared && enemy.childCount == 0)
+        {
+            ClearRoom();
+        }
     }
 }

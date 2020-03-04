46c17d3351844d8271f3938a923cb9def960c868
Dhkim <kimdhyungg@gmail.com>
2020-03-04 10:59:15 +0900

    better gravity equation

--- Assets/Scripts/Map/Gravity.cs
+++ Assets/Scripts/Map/Gravity.cs
@@ -8,31 +8,42 @@ public class Gravity : MonoBehaviour
     public float gravityAdd = 4000;
     public float gravityMul = 1.1f;
     public Vector3 initialVelocity = Vector3.zero;
+    public Vector3 acceleration;
     public Vector3 velocity;
     public Vector3 shift;
+    public int timestep;
+
 
     void Start()
     {
-        gravity = 1000F;
+        gravity = 9.8F * 16;
         gravityAdd = 4000;
         gravityMul = 1.1F;
         initialVelocity = Vector3.zero;
         velocity = initialVelocity;
         shift = Vector3.zero;
+        acceleration = Vector3.zero;
+        timestep = 0;
     }
     
     public Vector3 Shift(float deltaTime)
     {
-        velocity.y -= gravity * deltaTime;
+        /*
+        velocity.y -= gravity * gravity * deltaTime;
         //gravity += gravityAdd * deltaTime;
-        gravity = gravityMul * gravity;
+        //gravity = gravityMul * gravity;
         shift = velocity * deltaTime;
         return shift;
+        */
+        velocity.y -= gravity * deltaTime;
+        velocity.y *= 1.01f;
+        return shift = velocity * deltaTime;
+
     }
 
     public void Reset()
     {
-        Start();
+        velocity = initialVelocity;
     }
 
 }

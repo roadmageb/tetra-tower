24e3528679e76d56dcb78780369e609cc022e7bb
Ryu JiSeok <roadmageb@naver.com>
2020-03-07 23:29:29 +0900

    실과 바늘 구현 중

--- Assets/Scripts/ItemScript/Weapons/WpNeedleAndThread.cs
+++ Assets/Scripts/ItemScript/Weapons/WpNeedleAndThread.cs
@@ -7,4 +7,21 @@ public class WpNeedleAndThread : Weapon
     public WpNeedleAndThread(ScriptableWeaponInfo info) : base(info)
     {
     }
+
+    public override void PlaySkill(int skillNum, int option)
+    {
+        Transform trns = PlayerController.Instance.transform;
+        switch (skillNum)
+        {
+            case 0:
+                Object.Instantiate(info.projectiles[0]).GetComponent<ProjBow>().SetProjectile(
+                    new SkillInfo(this, 0),
+                    trns.position + new Vector3(1, 1, 0),
+                    false,
+                    (int)trns.localScale.x);
+                break;
+            case 1:
+                break;
+        }
+    }
 }

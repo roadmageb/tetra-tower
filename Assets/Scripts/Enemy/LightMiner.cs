2bc242ffe5759b54a8df440d573cb09469be7528
jasson31 <jasson31@naver.com>
2020-03-13 17:22:18 +0900

    등대 광부 공격 딜레이 조정 및 항상 패트롤하도록 수정

--- Assets/Scripts/Enemy/LightMiner.cs
+++ Assets/Scripts/Enemy/LightMiner.cs
@@ -5,27 +5,25 @@ using UnityEngine;
 public class LightMiner : Enemy
 {
     [SerializeField] private GameObject floatFly;
-    private GameObject lightMinerAlert;
 
     public void AttackPattern1()
     {
-        if(transform.GetComponentsInChildren<FloatFly>().Length == 0)
+        Instantiate(floatFly, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
+        Instantiate(floatFly, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
+        Instantiate(floatFly, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
+    }
+
+    public override void TraceAction()
+    {
+        Patrol();
+        if (Time.time - lastAttackedTime > attackDelay && Vector3.Distance(rb.position, target.position) <= attackRange)
         {
-            Instantiate(floatFly, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
-            Instantiate(floatFly, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
-            Instantiate(floatFly, transform.position + new Vector3(0, 1, 0), Quaternion.identity, transform);
+            AttackStart();
         }
     }
-
-    protected override void Start()
+    public override void AttackAction()
     {
-        base.Start();
-        lightMinerAlert = transform.Find("LightMinerAlert").gameObject;
-    }
-
-    protected override void Update()
-    {
-        base.Update();
-        lightMinerAlert.SetActive(DetectPlayer());
+        base.AttackAction();
+        Patrol();
     }
 }

500105565c6f8c699d3d7732cab451562811a0a0
jasson31 <jasson31@naver.com>
2020-03-20 15:39:26 +0900

    키 UI 구현 완료

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
     public Transform lifeStoneUI;
     public Transform miniMap;
+    public Text keyCountText;
+
+    private void LateUpdate()
+    {
+        keyCountText.text = PlayerController.Instance.keyAmount.ToString();
+    }
 }
